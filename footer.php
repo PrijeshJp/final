307018bfe5bfd6f64346ea42f7adc1d39dd90234
Jp1324 <mishaln3@gmail.com>
2017-12-20 12:02:37 -0800

    'UI 2

---
+++ footer.php
@@ -0,0 +1,65 @@
+        <!--========== FOOTER ==========-->
+        <footer class="g-bg-color--dark">
+            <div class="container g-padding-y-50--xs">
+                <div class="row">
+                    <div class="col-xs-6 ">
+                        <ul class="list-inline s-header__action">
+                            <li class="s-header__action-item">
+                                <a class="s-header__action-link clr" href="#">
+                                    <i class="g-padding-r-5--xs ti-facebook"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm">Facebook</span>
+                                </a>
+                            </li>
+                            <li class="s-header__action-item">
+                                <a class="s-header__action-link clr" href="#">
+                                    <i class="g-padding-r-5--xs ti-twitter"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm ">Twitter</span>
+                                </a>
+                            </li>
+                            <li class="s-header__action-item">
+                                <a class="s-header__action-link clr" href="#">
+                                    <i class="g-padding-r-5--xs ti-instagram"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm">Instagram</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-6 g-text-right--xs">
+                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white-opacity-light">© 2016 Minuscule Technologies Pvt. Ltd.. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- End Copyright -->
+        </footer>
+                <!--========== END FOOTER ==========-->
+
+        <!-- Back To Top -->
+        <a href="javascript:void(0);" class="s-back-to-top js__back-to-top"></a>
+
+        <!--========== JAVASCRIPTS (Load javascripts at bottom, this will reduce page load time) ==========-->
+        <!-- Vendor -->
+        <script type="text/javascript" src="vendor/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.migrate.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.smooth-scroll.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.back-to-top.min.js"></script>
+        <script type="text/javascript" src="vendor/scrollbar/jquery.scrollbar.min.js"></script>
+        <script type="text/javascript" src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="vendor/swiper/swiper.jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/waypoint.min.js"></script>
+        <script type="text/javascript" src="vendor/counterup.min.js"></script>
+        <script type="text/javascript" src="vendor/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.parallax.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBsXUGTFS09pLVdsYEE9YrO2y4IAncAO2U"></script>
+        <script type="text/javascript" src="vendor/jquery.wow.min.js"></script>
+
+        <!-- General Components and Settings -->
+        <script type="text/javascript" src="js/global.min.js"></script>
+        <script type="text/javascript" src="js/components/header-sticky.min.js"></script>
+        <script type="text/javascript" src="js/components/scrollbar.min.js"></script>
+        <script type="text/javascript" src="js/components/magnific-popup.min.js"></script>
+        <script type="text/javascript" src="js/components/swiper.min.js"></script>
+        <script type="text/javascript" src="js/components/counter.min.js"></script>
+        <script type="text/javascript" src="js/components/portfolio-3-col.min.js"></script>
+        <script type="text/javascript" src="js/components/parallax.min.js"></script>
+        <script type="text/javascript" src="js/components/wow.min.js"></script>
