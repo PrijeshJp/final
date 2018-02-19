e687822c338990ca5f09aaa6985a49372caa89a2
PrijeshJp <prijesh@minusculetechnologies.com>
2018-02-19 16:25:28 +0530

    new team page inprogress

---
+++ team_new.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minuscule Technologies - Salesforce Consulting, OutSystems Development , Digital marketing, OBIEE development outsourcing
+        experts</title>
+    <link rel="shortcut icon" href="favicon.ico" />
+    <meta name="description" content="Experts from Minuscule Technologies offers consulting and development solutions on Salesforce CRM, OutSystems platform on PaaS, digital marketing and strategy solutions on both on-shore and off-shore model.">
+    <meta name="author" content="">
+    <link rel="canonical" href="http://www.minusculetechnologies.com/about-us.html" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Salesforce CRM, OutSystems, Digital marketing and strategy solutions, OBIEE development outsourcing experts - Minuscule Technologies"
+    />
+    <meta property="og:description" content="Expert consulting and development solutions on Salesforce CRM, OutSystems platform on PaaS, digital marketing and strategy solutions on both on-shore and off-shore model from Minuscule Technologies "
+    />
+    <meta property="og:url" content="http://www.minusculetechnologies.com/about-us.html" />
+    <meta charset="utf-8">
+    <?php
+        include "script.php";
+    ?>
+</head>
+<!-- Body -->
+
+<body>
+        <?php
+            include ("nav.php");
+        ?>
+        <!--========== PROMO BLOCK ==========-->
+        <div class="backdrop">
+            <img src="img/400x400/team.jpg" class="img-responsive res backdrop">
+        </div>
+        <!-- <div class="backdrop img-responsive res" style="background: url('img/400x400/team.jpg'); width:1920px; height: 500px;">
+
+        </div> -->
+        <!--========== END PROMO BLOCK ==========-->
+
+        <!--========== PAGE CONTENT ==========-->
+        <div class="container" style="padding-top:10vh; padding-bottom:10vh;">
+            <div class="container-fluid bg-3 text-center">
+                <br>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                </div>
+            </div>
+            <br>
+
+            <div class="container-fluid bg-3 text-center">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Some text..</p>
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--========== END PAGE CONTENT ==========-->
+
+        <!--========== FOOTER ==========-->
+        <?php
+        include "footer.php";
+        ?>
+</body>
+<!-- End Body -->
+
+</html>
