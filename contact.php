e103dff6be669feffffc9dcc867f5f40a0d18f82
PrijeshJp <prijesh@minusculetechnologies.com>
2019-09-07 12:03:29 +0530

    Contact updated

--- contact.php
+++ contact.php
@@ -27,7 +27,8 @@
 					<div class="row">
 						<div class="col-sm-12 text-center">
 							<div class="gmap">
-<iframe allowfullscreen="" height="450" width="100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1944.141259471195!2d80.17241835222815!3d12.953765126954078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525ff2e4be8ad7%3A0x17ba6abc6b67e199!2sMinuscule+Technologies!5e0!3m2!1sen!2sin!4v1559537276096!5m2!1sen!2sin" style="border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px" width="600"></iframe>
+<!-- <iframe allowfullscreen="" height="450" width="100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1944.141259471195!2d80.17241835222815!3d12.953765126954078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525ff2e4be8ad7%3A0x17ba6abc6b67e199!2sMinuscule+Technologies!5e0!3m2!1sen!2sin!4v1559537276096!5m2!1sen!2sin" style="border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px" width="600"></iframe> -->
+                                <iframe allowfullscreen="" height="450" width="100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.2826303353445!2d80.17125931482184!3d12.95375799086759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5260e06a33ce31%3A0x39be3744de608b18!2sMinuscule%20Technologies%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1567764079007!5m2!1sen!2sin" style="border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px" width="600"></iframe>
 							</div>
 						</div>
 					</div>
@@ -74,6 +75,117 @@
             </div>
         </section>
         <!-- End Feedback Form -->
+            <!-- Feedback Form -->
+    <section id="contact-page">
+    <div class="container">
+        <div class="center">
+            <h2>How can we help you?</h2>
+            <p class="lead">Please give us your details and we will get back to you soon</p>
+        </div>
+        <div class="row contact-wrap">
+            <div class="status alert alert-success" style="display: none">&nbsp;</div>
+            <form action="sendemail.php" class="contact-form" id="main-contact-form" method="post" name="contact-form">
+                <div class="col-sm-5 col-sm-offset-1">
+                    <div class="form-group">
+                        <label>Name <em>*</em></label>
+                        <input class="form-control required nameReq" autocomplete="off" onKeyUp="errorRemove(this);" name="name" type="text" />
+                        <span class="contError">Please Enter Your Name</span>
+                    </div>
+                    <div class="form-group">
+                        <label>Email <em>*</em></label>
+                        <input class="form-control required emailReq" autocomplete="off" onKeyUp="errorRemove(this);" name="email" type="text" />
+                        <span class="contError">Please Enter Your Email</span>
+                    </div>
+                    <div class="form-group">
+                        <label>Phone</label>
+                        <input class="form-control" type="phone" name="phone" />
+                    </div>
+                    <div class="form-group">
+                        <label>Company Name</label>
+                        <input class="form-control" type="text" name="company" />
+                    </div>
+                </div>
+                <div class="col-sm-5">
+                    <div class="form-group">
+                        <label>Subject <em>*</em></label>
+                        <input class="form-control required subjectReq" autocomplete="off" onKeyUp="errorRemove(this);" name="subject" type="text" />
+                        <span class="contError">Please Enter Subject</span>
+                    </div>
+                    <div class="form-group">
+                        <label>Message <em>*</em></label>
+                        <textarea class="form-control required messageReq" autocomplete="off" onKeyUp="errorRemove(this);" id="message" name="message" rows="8"></textarea>
+                        <span class="contError cont">Please Enter Message</span>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary btn-lg" name="submit" type="submit" >Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!--/.row-->
+    </div>
+    <!--/.container-->
+</section>
+<script>
+    // Contact form
+	var form = $('#main-contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		
+		var is_valid = true;
+		
+		if($('.nameReq').val()==''){
+			is_valid = false;
+			$('.nameReq').next('.contError').slideDown();
+		} 
+		if($('.subjectReq').val()==''){
+			is_valid = false;
+			$('.subjectReq').next('.contError').slideDown();
+		} 
+		if($('.messageReq').val()==''){
+			is_valid = false;
+			$('.messageReq').next('.contError').slideDown();
+		}
+		if($('.emailReq').val()==''){
+			is_valid = false;
+			$('.emailReq').next('.contError').slideDown();
+		} else {
+			var email = $('.emailReq').val();
+			if(isValidEmailAddress(email)) {
+                $('.emailReq').next('.contError').slideUp;
+            } else {
+				is_valid = false;
+               $('.emailReq').next('.contError').slideDown().html("Please Enter a valid Email Address");
+            }
+		}
+		if(is_valid)
+			validateComplete(form);
+	});
+    
+    function errorRemove(that){
+	$(that).next('span').slideUp();
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+}
+
+function validateComplete(form){
+	var form_status = $('<div class="form_status center"></div>');
+	$.ajax({
+		url: $(form).attr('action'),
+		type: 'post',
+               data: $(form).serialize(),
+		beforeSend: function(){
+			form.prepend( form_status.html('<p class="mailSendingLoader"><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+		}
+	}).done(function(data){
+		form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).slideUp();
+               $('.form-control').delay(3000).val('');
+	});
+}
+</script>
         <!--========== END PAGE CONTENT ==========-->
         </footer>
         <!--========== END FOOTER ==========--> 
